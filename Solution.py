1763bfdc2907d4e0a52c65a67948827ac74a82d8
madhan-0407 <madhanrvmp7867@gmail.com>
2025-06-28 10:38:36 +0530

    Intial commit

---
+++ Solution.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    def sample(self,h):
+        return h
+    def getSneakyNumbers(self, nums: List[int]) -> List[int]:
+        self.sample("Hello")
+        return nums
+
+
+        
+sol=Solution()
+res=sol.getSneakyNumbers(nums = [7,1,5,4,3,4,6,0,9,5,8,2])
+print(res)
+        
