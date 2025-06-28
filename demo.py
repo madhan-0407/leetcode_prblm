2115556e59af762cdbcaec4f92ddea8c4fad3c94
madhan-0407 <madhanrvmp7867@gmail.com>
2025-06-28 10:47:38 +0530

    modify the demo .py

--- demo.py
+++ demo.py
@@ -1,1 +1,14 @@
-print("world github")
+
+from typing import List
+
+class Solution:
+    def sample(self,h):
+        return h
+    def getSneakyNumbers(self, nums: List[int]) -> List[int]:
+        self.sample("Hello")
+        return nums
+
+
+        
+sol=Solution()
+res=sol.getSneakyNumbers(nums = [56,455,5,525,11])
