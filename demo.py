8c53b7d2538a2d3250e17c9a1288288e51b8fdf3
madhan-0407 <madhanrvmp7867@gmail.com>
2025-06-29 14:23:52 +0530

    Solved

--- demo.py
+++ demo.py
@@ -1,11 +1,2 @@
-
-from typing import List
-
-class Solution:
-    def sample(self,h):
-        return h
-    def getSneakyNumbers(self, nums: List[int]) -> List[int]:
-        self.sample("Hello")
-        return nums
-sol=Solution()
-res=sol.getSneakyNumbers(nums = [56,455,5,525,11])
+s = ["h","e","l","l","o"]
+print(s.reverse())
