72870773d8c19da9b2093b71cda6929fe35d7344
madhan-0407 <madhanrvmp7867@gmail.com>
2025-07-01 16:13:32 +0530

    Not Solved!!!

---
+++ 84. Largest Rectangle in Histogram.py
@@ -0,0 +1,42 @@
+from typing import List
+
+
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        list_of_rectangle = []
+
+        heightest_length = max(heights)
+
+        for i in heights:
+            list_of_rectangle.append(set(range(1, i + 1)))
+
+        # Make separate copies so modifying one doesn't affect the other
+        list_of_rectangle1 = list_of_rectangle.copy()
+        list_of_rectangle2 = list_of_rectangle.copy()
+
+        while True:
+            if not list_of_rectangle1:
+                break
+            all_intersect = set.intersection(*list_of_rectangle1)
+            if len(all_intersect) * len(list_of_rectangle1) > heightest_length:
+                heightest_length = len(all_intersect) * len(list_of_rectangle1)
+            if len(list_of_rectangle1) == 1:
+                break
+            # Remove last element (since max(set) won't work meaningfully)
+            list_of_rectangle1.pop()
+
+        while True:
+            if not list_of_rectangle2:
+                break
+            all_intersect = set.intersection(*list_of_rectangle2)
+            if len(all_intersect) * len(list_of_rectangle2) > heightest_length:
+                heightest_length = len(all_intersect) * len(list_of_rectangle2)
+            if len(list_of_rectangle2) == 1:
+                break
+            # Remove first element (since min(set) won't work meaningfully)
+            list_of_rectangle2.pop(0)
+
+        return heightest_length
+
+
+print(Solution().largestRectangleArea(heights=[2, 1, 5, 6, 2, 3]))
